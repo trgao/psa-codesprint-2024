b544daa4c6aef7c0c6beb71e7adbdb1a2c8413e2
trgao <112098096+trgao@users.noreply.github.com>
2024-10-12 21:40:52 +0200

    Add navbar to dashboard

--- frontend/app/(main)/dashboard/page.tsx
+++ frontend/app/(main)/dashboard/page.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link'
 import { signOut } from './actions'
 
 export default async function Dashboard() {
   return (
     <div>
-      <form action={signOut}><button type="submit">Sign Out</button></form>
-      <p>Hello world</p>
+      <nav className="w-screen fixed flex justify-between align-middle">
+        <Link href="/dashboard" className="text-blue-600 font-medium text-2xl align-middle p-3">MentorShip</Link>
+        <form action={signOut} className="p-3">
+          <button type="submit" className="bg-blue-600 rounded-xl w-24 h-9 text-white">Sign Out</button>
+        </form>
+      </nav>
+      
     </div>
   )
 }
