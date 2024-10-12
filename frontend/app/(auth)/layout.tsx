02ccc9f99ea33c95bd8dcd781597ee51f7cf5ec8
trgao <112098096+trgao@users.noreply.github.com>
2024-10-12 17:03:44 +0200

    Add login page to frontend

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -1,17 +1,25 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
-  title: "Website",
+  title: "Login",
 };
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {  
+}>) {
+  const supabase = createClient()
+
+  const { data: { user } } = await supabase.auth.getUser()
+
+  if (user) {
+    redirect("/dashboard")
+  }
+
   return (
     <html lang="en">
       <body
---
+++ frontend/app/(auth)/login/actions.tsx
@@ -0,0 +1,39 @@
+"use server"
+
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+
+export async function signIn(formData: FormData) {
+  const supabase = createClient()
+
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    console.log(error)
+  }
+
+  redirect('/dashboard')
+}
+
+export async function signUp(formData: FormData) {
+  const supabase = createClient()
+
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    console.log(error)
+  }
+
+  redirect('/dashboard')
+}
