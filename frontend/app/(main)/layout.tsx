f3e96825e9d4bab8682d911eba4477d7973d65b5
trgao <112098096+trgao@users.noreply.github.com>
2024-10-13 16:16:56 +0200

    Fix ui styling

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import "../globals.css";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import Link from "next/link";
+import styles from './Dashboard.module.css'
 import { signOut } from "./actions";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "MentorShip - Dashboard",
@@ -27,13 +28,21 @@ export default async function RootLayout({
       <body
         className={`antialiased`}
       >
-        {/* <nav className="w-screen fixed flex justify-between align-middle">
-          <Link href="/dashboard" className="text-blue-600 font-medium text-2xl align-middle p-3">MentorShip</Link>
-          <form action={signOut} className="p-3">
-          <button type="submit" className="bg-blue-600 rounded-xl w-24 h-9 text-white">Sign Out</button>
-          </form>
-        </nav> */}
-        {children}
+        <div className={styles.dashboard}>
+          <div className={styles.sidebar}>
+            <div className={styles.logoContainer}>
+              <Image src="/logo.png" alt="MentorShip Logo" width={100} height={50} />
+            </div>
+            <div className={styles.brand}>MentorShip</div>
+            <div className={styles.welcome}>Welcome, {user.email}!</div>
+            <form action={signOut}><button className={styles.signout} type="submit">Sign Out</button></form>
+          </div>
+          <div className={styles.mainContent}>
+            <div className={styles.cardContainer}>
+              {children}
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
